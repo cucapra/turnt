d476b2a2b4b15eca75b0a6057f79c1cfb58985f9
Max Bernstein <max@bernsteinbear.com>
2025-01-07 19:11:31 -0500

    Remove click in favor of argparse

--- turnt/__main__.py
+++ turnt/__main__.py
@@ -1,4 +1,4 @@
-import click
+import argparse
 import contextlib
 import os
 import re
@@ -520,37 +520,78 @@ def run_tests(cfg: Config, parallel: bool, test_files: List[str]) -> bool:
         return success
 
 
-@click.command(context_settings=CONTEXT_SETTINGS)
-@click.option('--save', is_flag=True, default=False,
-              help='Save new outputs (overwriting old).')
-@click.option('--diff', is_flag=True, default=False,
-              help='Show a diff between the actual and expected output.')
-@click.option('-p', '--print', 'dump', is_flag=True, default=False,
-              help="Just show the command output (don't check anything).")
-@click.option('-v', '--verbose', is_flag=True, default=False,
-              help='Do not suppress stderr from successful commands.')
-@click.option('-a', '--args',
-              help='Override arguments for test commands.')
-@click.option('-j', '--parallel', is_flag=True,
-              help='Run tests in parallel.')
-@click.option('-c', '--config', default=CONFIG_NAME,
-              help=f'Name of the config file. Default: {CONFIG_NAME}')
-@click.option('-e', '--env', multiple=True,
-              help='The names of configured environment(s) to run.')
-@click.argument('file', nargs=-1, type=click.Path(exists=True))
-def turnt(file: List[str], save: bool, diff: bool, verbose: bool, dump: bool,
-          args: Optional[str], parallel: bool, config: str,
-          env: List[str]) -> None:
-    cfg = Config(
-        config_name=config,
-        save=save,
-        diff=diff,
-        verbose=verbose,
-        dump=dump,
-        args=args,
-        envs=env,
+def readable_file(name: str) -> str:
+    if os.path.isdir(name):
+        return name
+    with open(name, "r") as _:
+        pass
+    return name
+
+
+def turnt() -> None:
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--save",
+        action="store_true",
+        default=False,
+        help="Save new outputs (overwriting old).",
     )
-    success = run_tests(cfg, parallel, file)
+    parser.add_argument(
+        "--diff",
+        action="store_true",
+        default=False,
+        help="Show a diff between the actual and expected output.",
+    )
+    parser.add_argument(
+        "-p",
+        "--print",
+        action="store_true",
+        default=False,
+        help="Just show the command output (don't check anything).",
+    )
+    parser.add_argument(
+        "-c",
+        "--config",
+        default=CONFIG_NAME,
+        help=f"Name of the config file. Default: {CONFIG_NAME}",
+    )
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        action="store_true",
+        default=False,
+        help="Do not suppress stderr from successful commands.",
+    )
+    parser.add_argument(
+        "-a",
+        "--args",
+        help="Override arguments for test commands.",
+    )
+    parser.add_argument(
+        "-j",
+        "--parallel",
+        action="store_true",
+        default=False,
+        help="Run tests in parallel.",
+    )
+    parser.add_argument(
+        "-e",
+        "--env",
+        nargs="+",
+        help="The names of configured environment(s) to run.",
+    )
+    parser.add_argument("file", type=readable_file, nargs="+")
+    args = parser.parse_args()
+    cfg = Config(
+        config_name=args.config,
+        save=args.save,
+        diff=args.diff,
+        verbose=args.verbose,
+        dump=args.print,
+        args=args.args,
+        envs=args.env,
+    )
+    success = run_tests(cfg, args.parallel, args.file)
     sys.exit(0 if success else 1)
 
 
